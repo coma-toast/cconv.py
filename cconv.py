c3d25c200efb0c8780e80c240f7c8b721b81c25a
Jason Dale <jdale215@gmail.com>
2017-09-26 14:47:05 -0400

    adding all the objects we want to print, and cleanup

--- cconv.py
+++ cconv.py
@@ -5,40 +5,16 @@ import urllib2
 import json
 from pprint import pprint
 
-
-#import requests
-
-
-#coinmarketcap is weird, i'll try it again later
-#from coinmarketcap import Market
-
-#coinmarketcap = Market()
-#currencyType = "USD"
-#coinmarketcap.ticker(currencyType, limit=3, convert='EUR')
-
-
 def printCoin(data, curReq):
     theJSON = json.loads(data)
-    #print theJSON
+    print "Price:"
+    print " $" + str(theJSON['price'][curReq])
 
-    #for i in theJSON["market_cap"]:
-
-    #This works, but dumps ALL currencies:
-    #print json.dumps(theJSON, indent=2, separators=(',', ':'))
-
-    #try to print JUST the currency type that is requested
-    #for i in ...???
-    #https://docs.python.org/2/library/json.html
-    #https://stackoverflow.com/questions/9093684/how-to-print-particular-json-value-in-python
-    #for symbol in theJSON:
-    #    json.dumps({})
-
-    #This also works, but dumps all currencies in one big text block
-    #print theJSON
     print "Market Cap:"
-    print "$", (theJSON['market_cap'][curReq])
+    print " $" + str(theJSON['market_cap'][curReq])
 
-
+    print "Volume:"
+    print " $" + str(theJSON['volume'][curReq])
 
 def main():
     urlData = "https://coinmarketcap-nexuist.rhcloud.com/api/"
@@ -51,7 +27,7 @@ def main():
         print "Received an error from server, cannot retrieve results " + str(webUrl.getcode())
     #prompt user for coin, currency
     coinReq = raw_input('What coin you want to see? ')
-    curReq = raw_input('What currency type? ')
+    curReq = raw_input('What currency type? ').lower()
     #combine main URL with the coin name into one string
     fullUrl = urlData + coinReq
     #open the full URL and then read the data, into a variable
@@ -60,8 +36,6 @@ def main():
     #print out data for the coin specified
     printCoin(data, curReq)
     #print coinData
-
-
 
 
 if __name__ == "__main__":
