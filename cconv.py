e126066913a18c79d1e600875a612a0471873b1e
Jason <jdale215@gmail.com>
2018-10-29 08:30:15 -0400

    Clean up some comments, whitespace, unused code

--- cconv.py
+++ cconv.py
@@ -19,21 +19,26 @@ def printCoin(data, curReq):
 def main():
     urlData = "https://coinmarketcap-nexuist.rhcloud.com/api/"
     webUrl = urllib2.urlopen(urlData)
-    #print (webUrl.getcode())
+    
     #I will eventually turn this into a try/except thingy
     if (webUrl.getcode() == 200):
         print "Connection successful."
     else:
         print "Received an error from server, cannot retrieve results " + str(webUrl.getcode())
+
     #prompt user for coin, currency
     coinReq = raw_input('What coin you want to see? ')
     curReq = raw_input('What currency type? ').lower()
+
     #combine main URL with the coin name into one string
     fullUrl = urlData + coinReq
+
     #open the full URL and then read the data, into a variable
     webUrl2 = urllib2.urlopen(fullUrl)
     data = webUrl2.read()
+
     #print out data for the coin specified
+    
     printCoin(data, curReq)
     #print coinData
 
